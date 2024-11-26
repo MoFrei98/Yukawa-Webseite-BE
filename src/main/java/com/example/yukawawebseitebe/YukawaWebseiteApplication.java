50f80482c20976581ccc8f4c8db822334b5b5881
Morris Freihoff <morris.freihoff@web.de>
2024-11-26 12:10:27 +0100

    initial commit

---
+++ src/main/java/com/example/yukawawebseitebe/YukawaWebseiteApplication.java
@@ -0,0 +1,15 @@
+package com.example.yukawawebseitebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YukawaWebseiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YukawaWebseiteApplication.class, args);
+
+        System.out.println("--- LAUNCHED YUKAWA WEBSITE ---");
+    }
+
+}
---
+++ src/test/java/com/example/yukawawebseitebe/YukawaWebseiteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yukawawebseitebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YukawaWebseiteBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
