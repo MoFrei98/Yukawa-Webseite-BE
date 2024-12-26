1cdfca1c42bad535532ccf164d6e1b6b800cad0d
MoFrei98 <morris.freihoff@web.de>
2024-12-26 23:07:10 +0100

    changed username and password

---
+++ src/main/java/com/example/yukawawebseitebe/models/pinboard/PinboardItemAttachment.java
@@ -0,0 +1,23 @@
+package com.example.yukawawebseitebe.models.pinboard;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "pinboard_item_attatchments")
+public class PinboardItemAttachment {
+    @Id
+    @Column(name = "uuid", nullable = false)
+    private String uuid;
+
+    @ManyToOne
+    @JoinColumn(name = "pinboard_item_uuid", referencedColumnName = "uuid")
+    private PinboardItem pinboardItem;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "path")
+    private String path;
+}
